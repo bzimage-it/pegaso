4da7db595923b1c4ffaac12253fa8cdfa4f8839c
Fabrizio Sebastiani <fabrizio@bzimage.it>
2021-06-09 23:12:33 +0200

    added options 'inc','dec','verbose' to 'update' feature

--- PHP-crud/crud.php
+++ PHP-crud/crud.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "config.php";
+require "config.php";
 
 $bad_req = 400;
 $no_auth = 401;
@@ -22,8 +22,28 @@ function check_and_return($what) {
     return $_GET[$what];
 }
 
+function return_if_def($what,$default) {
+    if(!isset($_GET[$what])) {
+        return $default;
+    }
+    return $_GET[$what];
+}
+
+
+function read_filename($filename) {
+    $myfile = fopen($filename, "r") or abort("Unable to read $var",$bad_req);
+    $size=filesize($filename);
+    if($size == 0) {
+        return "";
+    }else{
+        $val = fread($myfile,$size) or abort("cannot read on $var",$internal);
+        return $val;
+    }
+}
+
 $cmd = check_and_return('cmd');
 $pw = check_and_return('pwd');
+$verbose = return_if_def('verbose',"1");
 
 if ( $pw != $secret) {
     abort("bad password",$no_auth);
@@ -38,32 +58,53 @@ if($cmd == 'create') {
     }
     $myfile = fopen($filename, "w") or abort("Unable to create file!",$internal);
     fwrite($myfile,$val);
-    echo "CREATED $var = $val";
+    if($verbose) {
+        echo "CREATED $var = $val";
+    }else{
+        echo "$val";
+    }
 }
 
 if($cmd == 'read') {
     $var = check_and_return('var');
     $filename = "$var.var";
-    $myfile = fopen($filename, "r") or abort("Unable to read $var",$bad_req);
-    $size=filesize($filename);
-    if($size == 0) {
-        echo "";
-    }else{
-        $val = fread($myfile,$size) or abort("cannot read on $var",$internal);
-        echo "$val";
-    }
+    $val = read_filename($filename);
+    echo "$val";
 }
 
 if($cmd == 'update') {
+    /* note that an "update & read" effect in one shot can be get setting verbose=0 */
     $var = check_and_return('var');
     $filename = "$var.var";
-    $val = check_and_return('val');
     if(!file_exists($filename)) {
         abort("var do not exists: $var",$bad_req);
     }
+    /* only one of 'val' , 'inc' , 'dec' can be defined: */
+    $val = return_if_def('val',"");
+    $inc = return_if_def('inc',"");
+    $dec = return_if_def('dec',"");
+    $ndef = ( (int) (bool) $val ) + ( (int) (bool) $inc ) + ( (int) (bool) $dec ) ;
+    if ($ndef==0) {
+        abort("no option defined between: val, inc, dec",$bad_req);
+    }elseif($ndef>1) {
+        abort("incompatible option defined, only one between: val, inc, dec; $ndef defined in total",$bad_req);
+    }
+    if($inc or $dec) {
+        $curval = read_filename($filename);
+        if($inc) {
+            $val = $curval + $inc;
+        }
+        if($dec) {
+            $val = $curval - $dec;
+        }
+    }
     $myfile = fopen($filename, "w") or abort("Unable to write $var",$bad_req);
     fwrite($myfile,$val) or abort("cannot write on $var",$internal);
-    echo "UPDATED $var = $val";
+    if($verbose) {
+        echo "UPDATED $var = $val";
+    }else{
+        echo "$val";
+    }
 }
 
 if($cmd == 'delete') {
