ae7353fcc9490207b809a2a9e25703daeaaa313f
Fabrizio Sebastiani <fabrizio@bzimage.it>
2021-06-11 00:16:25 +0200

    support for numeric remote variable; with managint of active per-user monitoring of traffic and smart decrementation of elapsed time based on concrete traffic-consuming criteria under a certain threashold

--- user-net-access/user-net-access.sh
+++ user-net-access/user-net-access.sh
@@ -29,10 +29,13 @@ foutput=/tmp/netaccess-out.tmp
 fcode=/tmp/netaccess-code.tmp
 
 
+
 CMD="$1"
 function log() {
-    echo $(date --iso-8601=seconds)" $*"
+    echo "$$ "$(date --iso-8601=seconds)" $*"
 }
+
+trap 'log "Caught SIGUSR1 (debugging)"' SIGUSR1
 
 function abort() {
     log "abort: $1"
@@ -57,13 +60,15 @@ function load_conf() {
 
 	test -z "$UNA_CRUD_PWD"  && abort "undefined env UNA_CRUD_PWD"
 	test -z "$UNA_CRUD_URL"  && abort "undefined env UNA_CRUD_URL"
+	test -z "$UNA_IPTABLES_USER_CHAIN_PREFIX" && abort "undefined env UNA_IPTABLES_USER_CHAIN_PREFIX"
+	test -z "$UNA_IPTABLES_TRAFFIC_THRESHOLD_PER_MINUTE" && abort "undefined env UNA_IPTABLES_TRAFFIC_THRESHOLD_PER_MINUTE" 
 	return 0
     fi
     return 1
 }
 
 
-log "STARTING $0"
+log "STARTING $0 [ PID= $$ ]"
 
 load_conf "${PEGASO_SCRIPT_DIR}/${PEGASO_SCRIPT_FILE}" ||    
     load_conf "/etc/${PEGASO_SCRIPT_FILE}" ||
@@ -75,10 +80,18 @@ function filter() {
     return $?
 }
 
+function read_traffic() {
+    local USERNAME="$1"
+    local traffic_chain="${UNA_IPTABLES_USER_CHAIN_PREFIX}${USERNAME}"
+    iptables -L OUTPUT -n -v -x | grep $traffic_chain | awk -- '{print $2}'
+}
+
+
 function action() {
     local CMD="$1"
     local USERNAME="$2"
     local UNA_UID="$(id -u "$USERNAME")"
+    local traffic_chain="${UNA_IPTABLES_USER_CHAIN_PREFIX}${USERNAME}"
     log "action: $CMD on user $USERNAME (uid=$UNA_UID) ..."
     case "$CMD" in
 	disable)
@@ -101,8 +114,26 @@ function action() {
 		fi
 	    done
 	    ;;
+	create-user-chain)
+	    iptables -N $traffic_chain
+	    ;;
+	drop-traffic-monitor)
+	    ret=0
+	    # continue to drop rule until found them:
+	    while [ $ret == 0 ]; do
+		iptables -D OUTPUT -m owner --uid-owner $UNA_UID -j $traffic_chain 2> /dev/null
+		ret=$?
+	    done
+	    ;;
+	start-traffic-monitor|restart-traffic-monitor)
+	    action drop-traffic-monitor $USERNAME
+	    action install-traffic-monitor $USERNAME
+	    ;;
+	install-traffic-monitor)
+	    iptables -A OUTPUT -m owner --uid-owner $UNA_UID -j $traffic_chain
+	    ;;
 	show)
-	    iptables -L OUTPUT
+	    iptables -L OUTPUT -n -v	    
 	    ;;
 	*)
 	    abort "$CMD unknown"
@@ -124,9 +155,14 @@ fi
 
 rm -f "$foutput" "$fcode"
 
+for UNA_USER in $UNA_USERS; do
+    action create-user-chain $UNA_USER
+done
+
 while true; do    
     for UNA_USER in $UNA_USERS; do
-	var=$(eval "$UNA_CRUD_VAR_ENABLE_TEMPLATE")
+	
+	var=$(eval "$UNA_CRUD_VAR_TEMPLATE")
 	url="${UNA_CRUD_URL}?cmd=read&var=${var}&pwd=${UNA_CRUD_PWD}"
 	log GET: $url	
 	curl --silent "$url" -o $foutput -w "%{http_code}" > $fcode
@@ -135,34 +171,42 @@ while true; do
 	log "read | RESPONSE: " [$CODE] $OUT
 	case "$CODE" in
 	    200)
-		case "${OUT^^}" in
+		OUT="${OUT^^}"
+		case "${OUT}" in
 		    "DISABLED"|"DISABLE"|"N"|"NO")
 			action disable $UNA_USER
 			;;
 		    "ENABLED"|"ENABLE"|"Y"|"YES")
-			var=$(eval "$UNA_CRUD_VAR_TIME_TEMPLATE")
-			url="${UNA_CRUD_URL}?cmd=update&var=${var}&verbose=0&dec=${UNA_SLEEP_M}&pwd=${UNA_CRUD_PWD}"
-			log GET: $url
-			curl --silent "$url" -o $foutput -w "%{http_code}" > $fcode
-			CODE2=$(cat $fcode)
-			OUT2=$(cat $foutput)
-			log "update | RESPONSE: " [$CODE2] $OUT2
-			case "$CODE2" in
-			    200)
-				if [ "$OUT2" -gt 0 ] ; then
-				    log NOT EXPIRED time $OUT2
-				    action enable $UNA_USER
-
+			action enable $UNA_USER
+			;;
+		    *)
+			if [[ $OUT =~ ^[+-]?[0-9]+$ ]] ; then
+			    if [ "$OUT" -le 0 ]; then
+				action disable $UNA_USER
+			    else
+				action enable $UNA_USER
+				# if is a number, go in traffic monitoring and decrementing
+				# time-slot
+				traffic="$(read_traffic $UNA_USER)"
+				traffic_per_minute=0
+				test -n "$traffic" && let traffic_per_minute="$traffic / $UNA_SLEEP_M"
+				log "traffic=$traffic traffic_per_minute=$traffic_per_minute"
+				if [ -n "$traffic" -a "$traffic_per_minute" -gt "$UNA_IPTABLES_TRAFFIC_THRESHOLD_PER_MINUTE" ]; then
+				    log "traffic is over threadshold $UNA_IPTABLES_TRAFFIC_THRESHOLD_PER_MINUTE"		    
+				    url="${UNA_CRUD_URL}?cmd=update&var=${var}&verbose=0&dec=${UNA_SLEEP_M}&pwd=${UNA_CRUD_PWD}"
+				    log GET: $url
+				    curl --silent "$url" -o $foutput -w "%{http_code}" > $fcode
+				    CODE2=$(cat $fcode)
+				    OUT2=$(cat $foutput)
+				    log "update | RESPONSE: " [$CODE2] $OUT2
 				else
-				    log EXPIRED time $OUT2
-				    action disable $UNA_USER
+				    log "traffic is under threadshold $UNA_IPTABLES_TRAFFIC_THRESHOLD_PER_MINUTE"
 				fi
-				;;
-			    *)
-				log "update | username=$UNA_USER http response: $CODE, if no time specified, enable"
-				 action enable $UNA_USER
-				;;
-			esac		       
+				action restart-traffic-monitor $UNA_USER
+			    fi
+			else
+			    log "not a valid value for var: ${var} = $OUT"
+			fi
 			;;
 		    *)
 			log "username=$UNA_USER error value: $OUT"
@@ -175,5 +219,7 @@ while true; do
 	esac
     done
     log "going sleep for $UNA_SLEEP secs: zzzzz..."
-    sleep $UNA_SLEEP
+    # this "& + wait" is for allow debugging without spent time, use kill -SIGUSR1 to break sleep
+    sleep $UNA_SLEEP &
+    wait $!
 done
