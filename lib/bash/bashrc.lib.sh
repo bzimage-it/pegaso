f90562ef41b4e91e2151d21f2e350fe31eca2f19
Fabrizio Sebastiani <fabrizio@bzimage.it>
2021-10-15 21:55:41 +0200

    introduced fz command for manage $HOME dir and zip most recent files

--- lib/bash/bashrc.lib.sh
+++ lib/bash/bashrc.lib.sh
@@ -20,61 +20,78 @@ alias s='history -s '
 # $
 #
 #
-cd() { 
-   local d="$1" ; 
-   local n=
-   local i=
-   local str=
-   if [[ "$d" =~  ^-([0-9]+) ]]; then
+function cd() { 
+    local d="$1" ; 
+    local n=
+    local i=
+    local str=
+    if [[ "$d" =~  ^-([0-9]+) ]]; then
    	n="${d:1}"
 	for i in $(seq 1 "$n"); do
-   			str+="../"
+   	    str+="../"
    	done
 	echo go back step $n: $str
    	builtin cd "$str"
-   else
+    else
 	test -z "$d" && d="$HOME"; 
 	if [ -f "$d" ]; then 
-	      d="$(dirname "$d")";
+	    d="$(dirname "$d")";
 	      builtin cd "$d"
 	else
-	      builtin cd "$@"
+	    builtin cd "$@"
 	fi
 	
-   fi 
+    fi 
 }
 
 
-# Fast zip execution of last "used" files in $HOME.
-# Use $HOME as as temporary working directory 
-# if found use 7z, otherwhice use zip
-# Parameters:
-#    tag: postfix to the file name. 
-#    n  : number of most recent used file to zip.
-# with no params show last 10 used files in $HOME
-
 function fz() {
-    if [ $# != 2 ];then
-	echo "<tag> <n>"
-	#    echo $0 "<tag> <file1> <file2>"
- 	ls -lrt --hide='*~' $HOME | tail -n 10
+    N="$1"
+    test -z "$N" && N=10
+    local lscmd="ls -lrt";
+    test -z "$def" && def=10;
+    
+    if [ "$N" == '-h' -o "$N" == '--help' ]; then
+	cat <<- 'EOF'
+Fast ls & zip execution of last "used" files in $HOME.
+Use \$HOME as as temporary working directory 
+if found use 7z, otherwhice use zip
+
+ls \$HOME function:
+     fz <n>
+
+     if <n> is positive show last files in \$HOME in 'ls -l' style 
+     if <n> is negative show last files in \$HOME in 'ls -1' stype 
+            (suitable for xargs or packed in cmdline)
+
+Fast ZIP: 
+     fz <n> <tag>
+
+     <n>     number of file to zip
+     <tag>   postfix to the file name (timestamp used as prefix)
+
+EOF
 	return 0
     fi
     
-    TAG="$1"
-    N="$2"
-    ZIP="zip -9"
-    EXT="zip"
-
-    which 7z > /dev/null && ZIP="7z a -bd" && EXT=7z
-    
+    if [ $# != 2 ]; then
+        if [ "$N" -lt 0 ]; then
+            let def="-1*$def"
+            lscmd="ls -1rt"
+        fi
+        ${lscmd} --group-directories-first --hide='*~' $HOME | tail -n "$N";
+        return 0;
+    fi;
+    TAG="$2";
+    ZIP="zip -9";
+    EXT="zip";
+    which 7z > /dev/null && ZIP="7z a -bd" && EXT=7z;
     ( cd "$HOME" &&
-	  NOW=$(date +%Y-%m-%d-%H%M)
+	  NOW="$(date +%Y-%m-%d-%H%M)" &&
 	  FILES="$(ls -1rt --hide='*~' . | tail -n $N | xargs)" &&
-	      echo "putting $FILES into 7z ..." &&
-	      T="${NOW}-$TAG.$EXT" &&
-	      $ZIP "$T" $FILES &&
-	  echo "CREATED $HOME/$T" )
+	  echo "putting $FILES into 7z ..." &&
+	  T="${NOW}-$TAG.$EXT" &&
+	  $ZIP "$T" $FILES &&
+	  echo "CREATED: $HOME/$T" )
     return $?
 }
-
