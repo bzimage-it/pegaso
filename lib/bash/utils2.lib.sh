57b70732411c0b4f749fe96cfd62e8af0adaa782
Fabrizio Sebastiani <fabrizio@bzimage.it>
2021-11-06 22:18:12 +0100

    first final version of utils2 with tests

--- lib/bash/utils2.lib.sh
+++ lib/bash/utils2.lib.sh
@@ -1,69 +1,79 @@
 #!/bin/bash
 
-PEGASO_START_SCRIPT_PWD=$(pwd)
+# this file can be both "sourced" via bash source command or
+# included at the beginning of your script.
+# 
+# you may also be funny if you include "header.sh" too at the
+# very beginning of your script.
 
-# this is a code snipped:
-# from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-# to understand what directory it's stored in bash script itself
-
-PEGASO_SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$PEGASO_SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  PEGASO_DIR="$( cd -P "$( dirname "$PEGASO_SOURCE" )" && pwd )"
-  PEGASO_SOURCE="$(readlink "$PEGASO_SOURCE")"
-  [[ $PEGASO_SOURCE != /* ]] && PEGASO_SOURCE="$PEGASO_DIR/$PEGASO_SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-PEGASO_SCRIPT_DIR="$( cd -P "$( dirname "$PEGASO_SOURCE" )" && pwd )"
-PEGASO_SCRIPT_FILE=$(basename "$PEGASO_SOURCE")
-# end of snipped
-
-cd "$PEGASO_SCRIPT_DIR"
-cd .. || exit 1
-export PEGASO_ROOT="$(pwd)"
-
-PEGASO_PARENT_ROOT="$(dirname "$PEGASO_ROOT")"
-
-# echo "$PEGASO_ROOT"
-# echo "$PEGASO_PARENT_ROOT"
 
 ################# LOGGING FACILITY #####################
-# from https://www.ludovicocaldara.net/dba/bash-tips-4-use-logging-levels/
+# inspired from
+# https://www.ludovicocaldara.net/dba/bash-tips-4-use-logging-levels/
 #
 declare -A term_colors=( ["black"]='\033[0;30m' ["red"]='\033[0;31m' ["green"]='\033[0;32m' ["yellow"]='\033[0;33m' ["purple"]='\033[0;35m' ["reset"]='\033[0m' )
 ### from 0 (silent) to 6 (debug)
 declare -A log_level2n=( ["any"]=0 ["force"]=0 ["silent"]=0 ["sil"]=0 ["s"]=0 ["o"]=0 ["output"]=0 ["crit"]=1 ["critical"]=1 ["c"]=1 ["error"]=2 ["err"]=2 ["e"]=2 ["warning"]=3 ["warn"]=3 ["w"]=3 ["notif"]=4 ["notification"]=4 ["n"]=4 ["info"]=5 ["information"]=5 ["i"]=5 ["informational"]=5 ["debug"]=6 ["d"]=6)
-declare log_n2str=("OUTPUT" "CRITICAL" "ERROR" "WARNING" "NOTIFIC" "INFO" "DEBUG")
-declare log_n2pad=("  "     ""         "   "   " "       " "      "    " "   ")
+declare log_n2str=("OUT"      "CRIT"  "ERROR" "WARN"  "NOTIF" "INFO"  "DEBUG")
+declare log_n2strpad=("OUT  " "CRIT " "ERROR" "WARN " "NOTIF" "INFO " "DEBUG")
 declare log_n2color=("reset" "purple" "red" "yellow" "green" "reset" "reset")
-declare log_n2fd_1=(1 1 1 1 1 1 2) # write all to stdout, and debug to stderr;
-declare log_n2fd_2=(3 3 3 3 3 3 3) # write all to /dev/null
-# log_source_short_info is for optimize time
-unset log_source_short_info
+declare log_n2fd=(1 1 1 1 1 1 1) # write all to stdout
+unset log_source_short_info # for optimize execution time 
 declare -A log_source_short_info
-log_level=5
-log_source_info=N
-function log_level_assert() { # return numeric level on successful or 255 on fail; usedo to check if we have to log or not
-    local level="$1"   
+declare -i log_level=5 # set a starting level
+declare -u log_source_info=N # source code information enable/disable; Y | N
+declare -u log_on_bad_level=LOG # define the behavoir on log level param error; value: ABORT | FAIL | LOG (log as debug) | ERROR (log as error) | IGNORE
+declare -u log_color_mode=Y # set color mode for level printing; Y | N
+declare log_timestamp_format="%Y-%m-%d %H:%M:%S" # a date(1) format for logging
+# log_source_info; set to empty string to disable
+declare -u log_source_format="%s:%l %f" # contains format in % style to print source code; %s (source filename) %l (line number) %f (function name); if empty, disable source info print.
+function log_level_assert() { # say wheather a log shall be done or not; return numeric level of the given level on successful (do log) or >=250 on fail (do not log); 
+    local level="$1"
+    if test ! -v log_level2n[$level] ; then
+	case "$log_on_bad_level" in
+	    ABORT)
+		abort 1 "bad log level: $level"
+		;;
+	    FAIL)
+		return 254;
+		;;
+	    LOG)
+		log debug "error in given log level: $level"
+		return 254;
+		;;
+	    ERROR)
+		log error "error in given log level: $level"
+		return 254;
+		;;
+	    *)
+		return 250; # return error (to not to log) but do nothing
+		;;
+	esac
+    fi
     local n_level=${log_level2n[$level]?}
     if [ $log_level -ge $n_level -o $n_level == 0 ]; then
 	return $n_level # successfull
     fi
     return 255 # unsuccessfull, false
 }
-function log() { 
+function log() {
     log_level_assert "$1"
     local n_level=$?
-    test $n_level == 255 && return 0
+    test $n_level -ge 250 && return 0
     shift
-    local fd1=${log_n2fd_1[$n_level]?}
-    local fd2=${log_n2fd_2[$n_level]?}
-    if [ -n "$log_date_format" ]; then
-	echo -n $(date "+$log_date_format")"|" >&$fd1 >&$fd2
+    local fd1=${log_n2fd[$n_level]?}
+    if [ -n "$log_timestamp_format" ]; then
+	echo -n $(date "+$log_timestamp_format")"|" >&$fd1 
     fi
-    echo -e -n ${term_colors[${log_n2color[$n_level]}]}${log_n2str[$n_level]}${term_colors[${log_n2color[reset]}]}"${log_n2pad[$n_level]}|" >&$fd1 >&$fd2
+    if [ "$log_color_mode" == Y ]; then
+	echo -e -n ${term_colors[${log_n2color[$n_level]}]}"${log_n2strpad[$n_level]}"${term_colors[reset]}"|" >&$fd1
+    else
+	echo -n "${log_n2strpad[$n_level]}|" >&$fd1
+    fi
     if [ -n "${log_source_format}" ]; then
 	local k="${BASH_SOURCE[1]}"
-	local short="${log_source_short_info[$k]}"
-	if [ -z "$short" ]; then
+	local short=
+	if [ ! -v "log_source_short_info[$k]" ]; then
 	    short="$(basename "$k")"
 	    log_source_short_info[$k]="$short"
 	else
@@ -72,9 +82,9 @@ function log() {
 	local sinfo=${log_source_format/"%S"/"$short"}
 	sinfo=${sinfo/"%L"/"${BASH_LINENO[0]}"}
 	sinfo=${sinfo/"%F"/"${FUNCNAME[1]}"}
-	echo -n "$sinfo|" >&$fd1 >&$fd2  # $short:${BASH_LINENO[0]} ${FUNCNAME[1]}|"
+	echo -n "$sinfo|" >&$fd1  # $short:${BASH_LINENO[0]} ${FUNCNAME[1]}|"
     fi
-    echo "$@" >&$fd1 >&$fd2
+    echo "$@" >&$fd1 
 }
 function dumpvar () { for var in $@ ; do echo "$var=${!var}" ; done }
 function log_level () { # set current logging level or print it if not given
@@ -85,45 +95,21 @@ function log_level () { # set current logging level or print it if not given
 	echo ${log_n2str[$log_level]?}
     fi
 }
-function log_set_fd () { # primary|secondary <fd> [<level1> [<level2> ... ]] : set levels to log to the given new file descriptor id; if no level given, all levels are processed
-    local class="$1"
-    if [[ $class != "primary" && $class != "secondary" ]] ;then
-	log warn "log_set_fd: passed bad class=$class, ignoring"
-    fi
-    local fd="$2"
+function log_set_fd () { #  <fd> [<level1> [<level2> ... ]] : set levels to log to the given new file descriptor id; if no level given, all levels are processed
+    local fd="$1"
     if [[ ! $fd =~ ^[0-9]+$ ]]; then # if is a number
 	log warn "log_set_fd: passed fd=$fd not a number, ignoring"
     fi
-    shift
     shift
     local n=    
     if [ $# -gt 0 ]; then
 	for level in $*; do
 	    n=${log_level2n[$level]?}
-	    case $class in
-		primary)
-		    log_n2fd_1[$n]="$fd"
-		    ;;
-		secondary)
-		    log_n2fd_2[$n]="$fd"
-		    ;;
-	    esac
+	    log_n2fd[$n]="$fd"
 	done
     else # if no level, process all:
-	case $class in
-	    primary)
-		log_n2fd_1=($fd $fd $fd $fd $fd $fd $fd)
-		;;
-	    secondary)
-		log_n2fd_2=($fd $fd $fd $fd $fd $fd $fd)
-		;;
-	esac
+	log_n2fd=($fd $fd $fd $fd $fd $fd $fd)
     fi
-}
-function log_init() {
-    # needed for secondary log. fd 3 goes to /dev/null by default
-    exec 3>/dev/null
-    log debug "log_init(): fd2=$fd2"
 }
 ################# ABORT #####################
 function abort () {
@@ -137,12 +123,12 @@ function abort () {
     log critical "ABORT [exit code $code]" "$@" 
     exit $code
 }
-################# DEBUG AND STACK TRACE  #####################
+################# DEBUG AND STACK TRACE #####################
 # snipped from : 
 # http://stackoverflow.com/questions/685435/bash-stacktrace
 function unix_stack_trace () { # param is the log level
     log_level_assert "$1"
-    test $? == 255 && return 0
+    test $? -ge 250 && return 0
     
     local TRACE=""
     local CP=$$ # PID of the script itself [1]
@@ -165,19 +151,18 @@ function unix_stack_trace () { # param is the log level
         fi
         CP=$PP
     done    
-    echo "Unix process backtrace (PID+command line): $TRACE" >&2
+    log "$1" "Unix process backtrace (PID+command line): $TRACE"
     # echo -en "$TRACE" | tac | grep -n ":" # using tac to "print in reverse" [3]	
 }
 # snipped from : 
 # http://stackoverflow.com/questions/685435/bash-stacktrace
 function bash_stack_trace() { # param is the log level
     log_level_assert "$1"
-    test $? == 255 && return 0
-    
+    test $? -ge 250 && return 0    
     local STACK=""
     # to avoid noise we start with 1 to skip get_stack caller
     local i
-    local stack_size=${#FUNCNAME[1]}
+    local stack_size=${#FUNCNAME[@]}
     for (( i=1; i<$stack_size ; i++ )); do
         local func="${FUNCNAME[$i]}"
         [ x$func = x ] && func=MAIN
@@ -187,7 +172,7 @@ function bash_stack_trace() { # param is the log level
         STACK+=$'\n'"   "$func" "$src":"$linen
     done
     STACK+=$'\n';
-    echo "STACK TRACE of \$\$=$$ BASHPID=$BASHPID ${STACK}" >&2    
+    log "$1" "STACK TRACE of \$\$=$$ BASHPID=$BASHPID ${STACK}"
 }
 ################# TEMPORARY FILES + CLEANUP #####################
 unset mktemp_stack
@@ -195,17 +180,16 @@ declare mktemp_stack=()
 # this function execute mktemp(1) passing parameters
 # and store filename in order to be clen up at the end of the program.
 # first parameter is the variable to write the output of mktemp(1)
+# return created file name into WRAP_MKTEMP.
+# DO NOT USE THIS FUNCTION AS SUBSHELL LIKE $(wrap_mktemp) !!
 WRAP_MKTEMP=
 function wrap_mktemp() {
-    local var="$1"
-    local F
-    shift
     WRAP_MKTEMP=$(mktemp $*)
     mktemp_stack+=("$WRAP_MKTEMP")
     # echo "${mktemp_stack[@]}"
     log debug wrap_mktemp "$@" "-> $WRAP_MKTEMP"
 }
-function cleanup_temp() {
+function cleanup_temp() { 
     local F=
     for F in "${mktemp_stack[@]}"; do 
 	rm -rf "$F" && log debug "cleanup $F"
@@ -215,26 +199,18 @@ function cleanup_temp() {
 #####################################################################
 ####################### USER CONFIGURATION VARIABLES ################
 #####################################################################
+# set variables, if different from defaults (see comment above):
+log_timestamp_format="%H:%M:%S"
+log_source_format="%s:%l %f"
+log_on_bad_level=LOG
 
-# set a date format for logging, see command date(1)
-log_date_format="%Y-%m-%d %H:%M:%S"
-log_date_format="%H:%M:%S"
-# to disable date format for logging, set it to empty:
-# log_date_format=""
-
-# log_source_info contains format in % style to print source code
-# information: %S (source filename) %L (line number) %F (function name)
-# if empty, disable source info print. ONLY UPPERCASE LETTERS!
-log_source_format="%S:%L %F"
-# log_source_format=""
-
-# set log level:
+# set your log level:
 log_level debug
 # read log level back:
-# log silent $(log_level)
+log silent $(log_level)
 
 # another way to read it back:
-# log silent DUMP: $(dumpvar log_level)
+log silent DUMP: $(dumpvar log_level)
 
 # configure your 'traps' to exit cleanly:
 trap on_exit EXIT QUIT
@@ -250,11 +226,13 @@ function on_exit() {
     unix_stack_trace debug
 }
 
+# "set -u": althought not necessary is strongly suggested as defensive
+# programming policy to many functions usgin ${xxxxx?} syntax.
+# Also many bugs can be discovered easier.
+set -u
 
 ####################### BEGIN YOUR SCRIPT AFTER HERE ##################
-
 # DEMO CODE: 
-log_init 3
 
 log warn "this is a warning"
 log err "this is an error"
@@ -279,31 +257,47 @@ log warning "this is shoed 1"
 log info "XXXXXXXXXXXXXXX this is not logged"
 log_level debug
 
+# testing temporary files management:
+wrap_mktemp
+tmp1=$WRAP_MKTEMP
+wrap_mktemp -d
+tmp2=$WRAP_MKTEMP
+echo $tmp1 $tmp2
+echo ls -l $tmp1 $tmp2
+
 # now change some fd to log to different files:
-out=debug.log
-exec 4> $out
+exec 4> $tmp1
 log_set_fd 4 debug
+log_color_mode=N # disable color mode
 log notif "this is notification, do not goes to 4"
 log debug "debugging goes to 4"
 log_set_fd 4 debug notif info warn
 log warn "this warn goes to 4"
 log notif "this notif goes to 4"
 log info "this info goes to 4"
+log_color_mode=Y # re-enable color mode
 
-echo "-----$out----------"
-cat $out
+echo "----- $tmp1 ----------"
+cat $tmp1
 echo "---------------"
 
 log_set_fd 2 # now log all to stderr
 log silent "this silent goes to stderr"
 log warn "this warn goes to stderr"
+log_set_fd 1 # go back to log all to stdout
 
+# now testing different values for log_on_bad_level:
+log_level debug
 
-wrap_mktemp
-tmp1=$WRAP_MKTEMP
-wrap_mktemp -d
-tmp3=$WRAP_MKTEMP
-echo $tmp1 $tmp2
-echo ls -l $tmp1 $tmp2
+log_on_bad_level=ignore
+log eeeee1 "XXXXXXXXXXXXX this shall not logged"
+log_on_bad_level=log
+log eeeee2 "this shall be logged as bad level eeeee2 DEBUG"
+log_on_bad_level=error
+log eeeee3 "this shall be logged as bad level eeeee3 ERROR"
+log_on_bad_level=fail
+log eeeee4 "XXXXXXXXXX this shall not be logged"
+log_on_bad_level=abort
+log eeeee5 "XXXXXXXX this shall not be logged, but abort happens eeeee5"
 
-abort 3 "now abort"
+# abort 3 "now abort"
