cf8ae0834c87fb55d9d3517d59c844c5a8e5f8b0
Fabrizio Sebastiani <fabrizio@bzimage.it>
2022-10-23 23:28:09 +0200

    different fixes and improvements

--- to_be_managed/occurence_int.c
+++ to_be_managed/occurence_int.c
@@ -2,19 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX (234256000+20000)
+#ifndef MAX
+#define MAX 234256000UL
+#endif
+
+#define SIZE (MAX+20000)
 
 
-char test[MAX];
+unsigned char test[MAX];
 
 int main(int argc, char *argv[]) {
+  printf("BUFFER SIZE: %lu | %lu\n",MAX,sizeof(test));
   memset(test,0,sizeof(test));
-  int i=0;
-  int x;
-  while(scanf("%d",&x) ) {
+  unsigned long int x;
+  while(scanf("%lu\n",&x) == 1) {
+    // printf("> %d\n",x);
     test[x]++;
+    if(x>=MAX) {
+	printf("out of MAX: %lu\n",x);
+	exit(2);
+    }
     if(test[x] > 1) {
-      printf ("duplicated: %d\n",test[x]);
+      printf ("duplicated %lu : %d\n",x,test[x]);
       exit(1);
     }
   }
