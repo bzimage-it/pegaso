b71375ad13c091dde1e3a670d1284d586a11856e
Fabrizio Sebastiani <fabrizio@bzimage.it>
2022-10-27 15:42:13 +0200

    better src

--- to_be_managed/occurence_int.c
+++ to_be_managed/occurence_int.c
@@ -3,16 +3,23 @@
 #include <stdlib.h>
 
 #ifndef MAX
-#define MAX 234256000UL
+#define MAX 234256000LU
 #endif
+
+#ifndef CONTINUE
+#define CONTINUE 1
+#endif
+
 
 #define SIZE (MAX+20000)
 
+unsigned char test[SIZE];
 
-unsigned char test[MAX];
+
+#define abort(c) { if (!CONTINUE) {  exit(c);} } while(0)
 
 int main(int argc, char *argv[]) {
-  printf("BUFFER SIZE: %lu | %lu\n",MAX,sizeof(test));
+  printf("BUFFER SIZE: %lu | %lu\n",(unsigned long int) MAX,sizeof(test));
   memset(test,0,sizeof(test));
   unsigned long int x;
   while(scanf("%lu\n",&x) == 1) {
@@ -20,11 +27,11 @@ int main(int argc, char *argv[]) {
     test[x]++;
     if(x>=MAX) {
 	printf("out of MAX: %lu\n",x);
-	exit(2);
+	abort(2);
     }
     if(test[x] > 1) {
       printf ("duplicated %lu : %d\n",x,test[x]);
-      exit(1);
+      abort(1);
     }
   }
 }
