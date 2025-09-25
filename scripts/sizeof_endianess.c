711550e692ff93febbf64110d57321ba6d7cc07c
Fabrizio Sebastiani <fabrizio@bzimage.it>
2025-09-25 10:12:50 +0200

    comment eng; + fix casting

--- scripts/sizeof_endianess.c
+++ scripts/sizeof_endianess.c
@@ -5,7 +5,7 @@
 */
 
 #define CR() printf("\n");
-#define OFFSET(ident,offset) ( (int) (((char *)&ident)[offset] ))
+#define OFFSET(ident,offset) ( (unsigned char) (((unsigned char *)&ident)[offset] ))
 
 #define PSIZE(type)     printf("%-13s | %2ld\n",#type,sizeof(type))
 
@@ -24,21 +24,21 @@
 #define ENDIANESS(type) do { \
         type x; \
         switch(sizeof(type)) { \
-            case 4: \
+	    case 4: \
 		x = (type) 0x01020304U; \
-		printf("%02d %02d %02d %02d             | ", OFFSET(x,0) , OFFSET(x,1), OFFSET(x,2) , OFFSET(x,3)); \
+		printf("%02x %02x %02x %02x             | ", OFFSET(x,0) , OFFSET(x,1), OFFSET(x,2) , OFFSET(x,3)); \
 		ENDIANESS_PRINT( IS_EQ(x,0,0x4) && IS_EQ(x,1,0x3) && IS_EQ(x,2,0x2) && IS_EQ(x,3,0x1), \
 				  IS_EQ(x,0,0x1) && IS_EQ(x,1,0x2) && IS_EQ(x,2,0x3) && IS_EQ(x,3,0x4) ); \
 		break; \
 	    case 2: \
 		x = (type) 0x0102U; \
-		printf("%02d %02d                   | ", OFFSET(x,0) , OFFSET(x,1) ); \
+		printf("%02x %02x                   | ", OFFSET(x,0) , OFFSET(x,1) ); \
 		ENDIANESS_PRINT( IS_EQ(x,0,0x2) && IS_EQ(x,1,0x1) , \
 				 IS_EQ(x,0,0x1) && IS_EQ(x,1,0x2) ); \
 		break; \
 	    case 8: \
 	    	x = (type) 0x0102030405060708UL; \
-		printf("%02d %02d %02d %02d %02d %02d %02d %02d | ", OFFSET(x,0) , OFFSET(x,1) , OFFSET(x,2) , OFFSET(x,3), OFFSET(x,4) , OFFSET(x,5) , OFFSET(x,6) , OFFSET(x,7) ); \
+		printf("%02x %02x %02x %02x %02x %02x %02x %02x | ", OFFSET(x,0) , OFFSET(x,1) , OFFSET(x,2) , OFFSET(x,3), OFFSET(x,4) , OFFSET(x,5) , OFFSET(x,6) , OFFSET(x,7) ); \
 		ENDIANESS_PRINT(  IS_EQ(x,0,0x8) && IS_EQ(x,1,0x7) && IS_EQ(x,2,0x6) && IS_EQ(x,3,0x5) && IS_EQ(x,4,0x4) && IS_EQ(x,5,0x3) && IS_EQ(x,6,0x2) && IS_EQ(x,7,0x1) , \
 				  IS_EQ(x,0,0x1) && IS_EQ(x,1,0x2) && IS_EQ(x,2,0x3) && IS_EQ(x,3,0x4) && IS_EQ(x,4,0x5) && IS_EQ(x,5,0x6) && IS_EQ(x,6,0x7) && IS_EQ(x,7,0x8) ); \
 		break; \
