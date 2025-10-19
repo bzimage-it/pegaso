9c47f82e79e7284b07672e34fda991a8e1dbf85f
Fabrizio Sebastiani <fabrizio@bzimage.it>
2025-10-19 23:00:41 +0200

    applied GPL v3.0

--- webplayer/convert_audio.sh
+++ webplayer/convert_audio.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 #
 # Audio conversion script with custom structure and metadata
+# Copyright (C) 2025 Audio Player Contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Usage: ./convert_audio.sh <source-dir> <target-base> [--profile <profile>] [--fix-duration-seeking-aac] [permutation...]
 #
